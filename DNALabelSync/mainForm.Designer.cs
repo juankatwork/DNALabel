e1e32d9bd9f7bb1d8108394e8a7ef4bae6cdd1b1
juankatwork <juankatwork@hotmail.com>
2025-06-01 23:18:32 -0400

    updates to repo visibility test

--- DNALabelSync/mainForm.Designer.cs
+++ DNALabelSync/mainForm.Designer.cs
@@ -54,7 +54,7 @@ namespace DNALabelSync
             this.toolStripButtonLoadItemMaster});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(1067, 27);
+            this.toolStrip1.Size = new System.Drawing.Size(800, 27);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
             // 
@@ -63,63 +63,64 @@ namespace DNALabelSync
             this.toolStripButtonLoadSerialMatch.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonLoadSerialMatch.Image")));
             this.toolStripButtonLoadSerialMatch.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonLoadSerialMatch.Name = "toolStripButtonLoadSerialMatch";
-            this.toolStripButtonLoadSerialMatch.Size = new System.Drawing.Size(115, 24);
+            this.toolStripButtonLoadSerialMatch.Size = new System.Drawing.Size(96, 24);
             this.toolStripButtonLoadSerialMatch.Text = "Serial Match";
             this.toolStripButtonLoadSerialMatch.Click += new System.EventHandler(this.toolStripButtonLoadSerialMatch_Click);
             // 
             // toolStripSeparator1
             // 
             this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 31);
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 27);
             // 
             // toolStripButtonSettings
             // 
             this.toolStripButtonSettings.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSettings.Image")));
             this.toolStripButtonSettings.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonSettings.Name = "toolStripButtonSettings";
-            this.toolStripButtonSettings.Size = new System.Drawing.Size(86, 28);
+            this.toolStripButtonSettings.Size = new System.Drawing.Size(73, 24);
             this.toolStripButtonSettings.Text = "&Settings";
             this.toolStripButtonSettings.Click += new System.EventHandler(this.toolStripButtonSettings_Click);
             // 
             // toolStripSeparator2
             // 
             this.toolStripSeparator2.Name = "toolStripSeparator2";
-            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 31);
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 27);
             // 
             // toolStripButtonLoadAssemblyLine
             // 
             this.toolStripButtonLoadAssemblyLine.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonLoadAssemblyLine.Image")));
             this.toolStripButtonLoadAssemblyLine.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonLoadAssemblyLine.Name = "toolStripButtonLoadAssemblyLine";
-            this.toolStripButtonLoadAssemblyLine.Size = new System.Drawing.Size(127, 28);
+            this.toolStripButtonLoadAssemblyLine.Size = new System.Drawing.Size(107, 24);
             this.toolStripButtonLoadAssemblyLine.Text = "Assembly Line";
             this.toolStripButtonLoadAssemblyLine.Click += new System.EventHandler(this.toolStripButtonLoadAssemblyLine_Click);
             // 
             // toolStripSeparator3
             // 
             this.toolStripSeparator3.Name = "toolStripSeparator3";
-            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 31);
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 27);
             // 
             // toolStripButtonLoadItemMaster
             // 
             this.toolStripButtonLoadItemMaster.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonLoadItemMaster.Image")));
             this.toolStripButtonLoadItemMaster.ImageTransparentColor = System.Drawing.Color.Magenta;
             this.toolStripButtonLoadItemMaster.Name = "toolStripButtonLoadItemMaster";
-            this.toolStripButtonLoadItemMaster.Size = new System.Drawing.Size(112, 24);
+            this.toolStripButtonLoadItemMaster.Size = new System.Drawing.Size(94, 24);
             this.toolStripButtonLoadItemMaster.Text = "Item &Master";
             this.toolStripButtonLoadItemMaster.Click += new System.EventHandler(this.toolStripButtonLoadItemMaster_Click);
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.toolStrip1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "mainForm";
             this.Text = "DNA Label Match";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.mainForm_Load);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
