acbcbb4d431df9c74b9cf11c230f92c3b8636631
juankatwork <juankatwork@hotmail.com>
2025-06-06 09:48:08 -0400

    config and item master sql insert

---
+++ SQLScript/ItemMaster.Insert.sql
@@ -0,0 +1,15 @@
+USE [DNASerial]
+GO
+
+INSERT INTO [dbo].[ItemMaster]
+           ([Item Model Number]
+           ,[Description]
+           ,[UPC Code]
+           ,[Label Name]
+           ,[Engine Serial No. Identifier])
+     VALUES
+           ('LM-2119SP','Echo 21 in. 190 cc Gas Walk Behind Self-Propelled Lawn Mower',
+		   '743184026991','UPC','125P020089F1'),
+		   ('MNA153003','Description','','Label','09P7020294F1')
+
+
